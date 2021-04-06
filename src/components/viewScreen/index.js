dad6b675150516c3c064971f7ad145110615faca
zzh0c <zzh0czz@gmail.com>
2021-04-06 16:05:02 +0300

    feat: add switch

--- src/components/viewScreen/index.js
+++ src/components/viewScreen/index.js
@@ -3,24 +3,37 @@ import React, {useState} from "react";
 async function getData(setTest) {
     let url = 'https://api.mocki.io/v1/335b9fd3'
     let response = await fetch(url, {
-        headers : {
+        headers: {
             'Content-Type': 'application/json',
             'Accept': 'application/json'
         }
     });
 
     if (response.ok) {
-       await sortData(response,setTest);
+        await sortData(response, setTest);
     } else {
         alert("Ошибка HTTP: " + response.status);
     }
 }
 
-async function sortData(response,setTest) {
-    await response.json().then(function(data) {
-        console.log(data)
+async function sortData(response, setTest) {
+    await response.json().then(function (data) {
+        console.log()
         console.log(data.device.id)
-        setTest(data.device.id)
+
+        switch (Object.keys(data)[0]) {
+            case 'device':
+                setTest(data.device.id);
+                break;
+            case 'devices':
+                console.log(Object.keys(data)[0]);
+                break;
+            case 'Residentials':
+                console.log(Object.keys(data)[0]);
+                break;
+        }
+
+
     });
 }
 
@@ -32,7 +45,7 @@ export default function ViewScreen() {
         <div className={'view-screen_container'}>
             {test}
             <button
-                onClick={()=>{
+                onClick={() => {
                     getData(setTest)
                 }}
             >
